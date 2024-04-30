687ad33473629ca68d75c29e6fc8731f8a1551f2
Danielvanderheijden <daniel.vdheijden@shopmonkey.nl>
2024-04-30 16:19:43 +0200

    customer resource removed

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Models\Customer;
 use App\Models\subscription;
-use App\Http\Controllers\SendMailController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/customers/{id}', [SendMailController::class, 'index']);
+
 
 
